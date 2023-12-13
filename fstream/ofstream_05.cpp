f17d5ddeb180aacfeb73f73e326a9276c4645609
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-13 18:59:34 +0300

    Update ofstream_05.cpp

--- fstream/ofstream_05.cpp
+++ fstream/ofstream_05.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include "nutility.h"
 #include <iomanip>
 
+bool isprime(int val)
+{
+	if (val < 2)
+		return false;
+
+	if (val % 2 == 0)
+		return val == 2;
+
+	if (val % 3 == 0)
+		return val == 3;
+
+	if (val % 5 == 0)
+		return val == 5;
+
+	for (int k = 7; k * k <= val; k += 2)
+		if (val % k == 0)
+			return false;
+
+	return true;
+}
 
 int main()
 {
 	using namespace std;
-	
-	int n;
+
+	int n{};
 	cout << "ilk kac asal sayi: ";
 	cin >> n;
 	cout << "bir satirda kac sayi olsun: ";
-	int prime_per_line;
+	int prime_per_line{};
 	cin >> prime_per_line;
 
 	ostringstream oss;
 	oss << "asal" << n << "_" << prime_per_line << ".txt";
 	ofstream ofs{ oss.str() };
 	if (!ofs) { cerr << "olusturulamadi\n"; return 1; }
-	
+
 	int x = 2;
 	int prime_count = 0;
 
@@ -36,4 +55,3 @@ int main()
 		++x;
 	}
 }
-
