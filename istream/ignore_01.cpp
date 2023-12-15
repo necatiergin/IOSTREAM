78e7112ef04a3601e29cd69f91b9a6da13b7fe4f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-15 19:11:44 +0300

    Create ignore_01.cpp

---
+++ istream/ignore_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <limits>
+#include <sstream>
+
+constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
+
+int main()
+{
+    std::istringstream input(
+        "345necati ergin\n"
+        "976ahmet acar\n"
+        "4525ayse kale\n"
+        "781832mustafa aksoy\n");
+
+    for (;;)
+    {
+        int ival;
+        input >> ival;
+
+        if (input.eof() || input.bad())
+            break;
+        if (input.fail())
+        {
+            input.clear(); // unset failbit
+            input.ignore(max_size, '\n'); // skip bad input
+        }
+        else
+            std::cout << ival << '\n';
+    }
+}
