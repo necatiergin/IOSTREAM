30a9d7f5e65ea85921737c0a03f9c13dceeb5be1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-15 08:24:07 +0300

    Update mnp_08.cpp

--- formatting/manipulators/mnp_08.cpp
+++ formatting/manipulators/mnp_08.cpp
@@ -6,5 +6,7 @@ int main()
 
 	double dval = 237.645483;
 
-	cout << scientific << dval << "\n" << fixed << dval << "\n" << hexfloat << dval << "\n";
+	cout << scientific << dval << '\n' 
+		<< fixed << dval << '\n' 
+		<< hexfloat << dval << '\n';
 }
