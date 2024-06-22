8824afa96d7bd856e202755171f177092dceefee
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-22 08:51:05 +0300

    Update ifstream_03.cpp

--- fstream/ifstream/ifstream_03.cpp
+++ fstream/ifstream/ifstream_03.cpp
@@ -3,15 +3,6 @@
 
 int main()
 {
-	using namespace std;
-
-	ifstream ifs{ "temp.txt" };
-
-	//cout << ifs.rdbuf();#include <fstream>
-#include <iostream>
-
-int main()
-{
 	std::ifstream ifs{ "temp.txt" };
 	//code
 	//std::cout << ifs.rdbuf();
@@ -19,11 +10,5 @@ int main()
 	char c;
 	while (ifs.get(c)) {
 		std::cout.put(c);
-	}
-}
-
-	char c;
-	while (ifs.get(c)) {
-		cout.put(c);
 	}
 }
