fe4f1513e66e01e6a517e36725840d8bc517294e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-08 18:00:46 +0300

    Update fmt_07.cpp

--- formatting/fmt_07.cpp
+++ formatting/fmt_07.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	std::cout, std::setfill, std::setw;
+	using std::cout, std::setfill, std::setw;
 	int ival = 3451;
 	cout << setfill('*') << setw(12) << ival << "necati" << '\n';
 }
