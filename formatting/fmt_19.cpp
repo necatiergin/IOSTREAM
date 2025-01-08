0f64112906bca7e7508026e71d7d4b1826dff064
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-08 18:23:25 +0300

    Update and rename formatting/manipulators/mnp_09.cpp to formatting/fmt_19.cpp

--- formatting/fmt_19.cpp
+++ formatting/fmt_19.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
 
 int main()
 {
 	int x;
+
 	std::cout << "bir tamsayi giriniz (oct) : ";
-	cin >> oct >> x;
+	std::cin >> std::oct >> x;
 	std::cout << "x = " << x << '\n';
 	std::cout << "bir tamsayi giriniz (hex) : ";
-	cin >> hex >> x;
+	std::cin >> std::hex >> x;
 	std::cout << "x = " << x << '\n';
 }
