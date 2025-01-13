97a0a4155c6648704741a9534c1f2a0f1f0f7e78
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-13 17:40:52 +0300

    Update istream_iterator_03.cpp

--- istream_iterator/istream_iterator_03.cpp
+++ istream_iterator/istream_iterator_03.cpp
@@ -2,10 +2,10 @@
 #include <iterator>
 #include <numeric>
 
-using namespace std;
-
 int main()
 {
-	cout << "enter the numbers: " << 
-	accumulate(istream_iterator<int>{cin}, {}, 0) << '\n';
+	using namespace std;
+
+	cout << "enter the numbers: " <<
+		accumulate(istream_iterator<int>{cin}, {}, 0) << '\n';
 }
