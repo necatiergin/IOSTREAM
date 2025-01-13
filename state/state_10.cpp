7f92f8ce1afade5f57d09e4fb937f1d66a9c7e13
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-13 18:51:47 +0300

    Create state_10.cpp

---
+++ state/state_10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+
+
+void display_state(const std::ios& s)
+{
+	const auto stream_state = s.rdstate();
+	if (stream_state == 0) {
+		std::cout << "stream is OK\n";
+		return;
+	}
+
+	if (stream_state & std::ios::failbit) {
+		std::cout << "fail bit is set\n";
+	}
+
+	if (stream_state & std::ios::eofbit) {
+		std::cout << "eof bit is set\n";
+	}
+
+	if (stream_state & std::ios::badbit) {
+		std::cout << "bad bit is set\n";
+	}
+	std::cout << "============================\n";
+}
+
+
+int main()
+{
+	std::istringstream iss;
+
+	display_state(iss);
+	iss.clear(std::ios::eofbit);
+	display_state(iss);
+	iss.clear(std::ios::failbit);
+	display_state(iss);
+	iss.clear(std::ios::badbit);
+	display_state(iss);
+	iss.clear();
+	display_state(iss);
+}
