7a12447d214e4bff29e33bf2b50024edd424556f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-01-23 17:48:25 +0300

    Update rdbuf_05.cpp

--- streambuf/rdbuf_05.cpp
+++ streambuf/rdbuf_05.cpp
@@ -4,19 +4,17 @@
 
 int main()
 {
-	using namespace std;
-	
 	std::ofstream ofs{ "out.txt" };
 	if (!ofs) {
 		std::cerr << "out.txt dosyasi olusturulamadi\n";
 		exit(EXIT_FAILURE);
 	}
 
-	cout.rdbuf(ofs.rdbuf());
+	std::cout.rdbuf(ofs.rdbuf());
 
-	cout << "ali\n";
+	std::cout << "ali\n";
 	for (int i = 0; i < 10; ++i) {
-		cout << i << '\n';
+		std::cout << i << '\n';
 	}
 
 	ofs.close();
