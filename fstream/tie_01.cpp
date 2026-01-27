944a63a1a4ddebd6ee8fa8b21fd549ed89de36f5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-01-27 18:55:24 +0300

    Update tie_01.cpp

--- fstream/tie_01.cpp
+++ fstream/tie_01.cpp
@@ -2,13 +2,11 @@
 
 int main()
 {
-	using namespace std;
-
-	if (auto osptr = cin.tie()) {
-		cout << "cin is tied. the address of the stream cin tied to is : " << osptr << '\n';
-		cout << "&cout = " << &cout << '\n';
+	if (auto osptr = std::cin.tie()) {
+		std::cout << "cin is tied. the address of the stream cin tied to is : " << osptr << '\n';
+		std::cout << "&cout = " << &cout << '\n';
 	}
 	else {
-		cout << "cin not tied to any output stream\n";
+		std::cout << "cin not tied to any output stream\n";
 	}
 }
