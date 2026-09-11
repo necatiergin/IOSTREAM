82d3f35a2dac87c8f8651875254d31548f831284
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-09-11 17:35:28 +0300

    Add example of seeking in ostringstream
    Demonstrate usage of ostringstream and seekp.

---
+++ seek/seek_07.cpp
@@ -0,0 +1,15 @@
+
+#include <sstream>
+#include <iostream>
+
+
+int main()
+{
+	std::ostringstream oss;
+
+	oss << "0123456789";
+	oss.seekp(std::streamoff{ -5 }, std::ios::end);
+	oss << "ABC";
+
+	std::cout << oss.str() << '\n';  // 01234ABC89
+}
